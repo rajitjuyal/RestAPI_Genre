f9ac99019044b8e2d2178748ab6e8594ede2c56d
rjuyal123 <rajitjuyal@gmail.com>
2018-07-12 23:53:25 +0530

    added post rest api
    added post rest api

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const Joi= require('joi');
+app.use(express.json());
+
+
 console.log(`app object gets created`);
  app.get('/api/author',(req,res)=>{
  //res.sendStatus(200);
@@ -9,10 +13,10 @@ console.log(`app object gets created`);
 
  var genres =
  [
-     {name:"action"},
-     {name:"adventure"},
-     {name:"comdey"},
-     {name:"sci-fie"},
+     {id:1,name:"action"},
+     {id:2,name:"adventure"},
+     {id:3,name:"comdey"},
+     {id:4,name:"sci-fie"},
  ];
  
  app.get('/api/genre',(req,res)=>{
@@ -22,15 +26,29 @@ console.log(`app object gets created`);
     });
    
 
-    app.put('/api/genre/:name',(req,res)=>{
+    app.post('/api/genre',(req,res)=>{
         // res.sendStatus(200);
-        let name =app.param.name;
-        //need to think
-         res.send(genres);
-         res.end();
+       // let name =app.body.name;
+       const {error} = validateParam(req.body);
+       if(error){return res.status(400).send(error.details[0].message);}
+       console.log("Working fine ");
+       const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+      };
+        genres.push(genre);
+        res.send(genre);
          });
     
+function validateParam(genre){
+    const schema ={
+       name: Joi.string().min(3).required()
+    };
 
+    return Joi.validate(genre,schema);
+
+
+}
 const port = 38080;
  app.listen(port,()=> {console.log( `Node app is listening to ${port}`);});
  //app.listen(port);
